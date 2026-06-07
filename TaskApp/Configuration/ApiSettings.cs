4c479c7aa8103acb45d300b4c454912ba7e88964
AndrewRodman <AndrewRodman@users.noreply.github.com>
2026-06-07 14:08:36 -0400

    Point TaskApp at Azure API

--- TaskApp/Configuration/ApiSettings.cs
+++ TaskApp/Configuration/ApiSettings.cs
@@ -2,9 +2,5 @@ namespace TaskApp.Configuration;
 
 public static class ApiSettings
 {
-#if ANDROID
-    public const string BaseUrl = "http://10.0.2.2:5046/";
-#else
-    public const string BaseUrl = "http://localhost:5046/";
-#endif
+    public const string BaseUrl = "https://taskapi-andrew.azurewebsites.net/";
 }
